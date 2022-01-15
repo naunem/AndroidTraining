fc2ba78b1a362ef8a52192058fd14120d5fbc623
tupt <topbang1999@gmail.com>
2022-01-15 22:06:06 +0700

    xong bai 4

--- app/src/main/java/com/topbang/learnandroid/Excercise4.kt
+++ app/src/main/java/com/topbang/learnandroid/Excercise4.kt
@@ -1,6 +1,5 @@
 package com.topbang.learnandroid
 
-//Bài 1
 //Một đơn vị sản xuất gồm có các cán bộ là công nhân, kỹ sư, nhân viên. Mỗi cán bộ cần quản lý các dữ liệu:
 // Họ tên, tuổi, giới tính(name, nữ, khác), địa chỉ.
 //
