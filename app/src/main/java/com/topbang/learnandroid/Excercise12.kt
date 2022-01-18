17b9d45d7ac5180fa8bef0b9aef31d9a8f1b223e
tupt <topbang1999@gmail.com>
2022-01-18 10:45:40 +0700

    De Bai 11

---
+++ app/src/main/java/com/topbang/learnandroid/Excercise12.kt
@@ -0,0 +1,12 @@
+package com.topbang.learnandroid
+
+//Để xử lý văn bản người ta xây dựng lớp VanBan  có thuộc tính riêng là một xâu ký tự.
+//
+//Yêu cầu 1: Xây dựng hàm khởi tạo VanBan(), VanBan(String st).
+//
+//Yêu cầu 2: Xây dựng phương thức đếm số từ của văn bản.
+//
+//Yêu cầu 3: Xây dựng phương thức đếm số lượng ký tự A( không phân biệt hoa thường) của văn bản.
+//
+//Yêu cầu 4: Chuẩn hoá văn bản theo tiêu chuẩn sau: Ở đầu và cuối sâu không có ký tự trống,
+// ở giữa sâu không có 2 hoặc nhiều hơn các ký tự khoảng trắng kiền kề nhau.
