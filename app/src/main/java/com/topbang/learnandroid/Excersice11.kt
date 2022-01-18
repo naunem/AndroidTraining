10119554345af0b95dbbc18aad410c852f95fcf1
tupt <topbang1999@gmail.com>
2022-01-18 10:02:57 +0700

    bai tap 11

---
+++ app/src/main/java/com/topbang/learnandroid/Excersice11.kt
@@ -0,0 +1,11 @@
+package com.topbang.learnandroid
+
+//Bài tập tự luyện
+//Xây dựng lớp SoPhuc có các thuộc tính PhanThuc, PhanAo kiểu double.
+//Yêu cầu 1: Xây dựng các phương thức tạo lập
+//Yêu cầu 2: Xây dựng các phương thức:
+//Nhập một số phức.
+//Hiện thị số phức.
+//Cộng 2 số phức.
+//Nhân 2 số phức.
+
